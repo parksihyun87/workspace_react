282ea4cf29714ffdd99b739eb4a8596a126adc06
parksihyun87 <solarhyun87@gmail.com>
2025-04-01 22:52:21 +0900

    2025-04-01 save

--- ex-app/src/List.js
+++ ex-app/src/List.js
@@ -4,42 +4,27 @@ import { useDispatch, useSelector } from "react-redux";
 export default function List(){
     const dispatch=useDispatch();
     const arr=useSelector((state)=>state.counter.wishList);
-    // 스테이트 상, 네임, 이니셜 스테이트 안의 속성명
-    // 밑에는 알맞은 형식으로 써 준다.
-    // 처음은 state로 시작하고, 
-
-    
-    // 버튼을 누르면 으로 해당 내용이 실행되도록.
-    // 눌르면 set state의 배열안에 해당 내용을 넣어버림, 
-    // 긁어와야 하는 내용은 e.target의 내용들임
-    // 안의 내용을 긁어와서 li에 추가.
-    // 함수부의 내용은 본대에서 정의
-    // 일단 한줄이라도 넣어 볼 것.
-    // for문으로 돌려서 li를 만들고 넣어서 발행.
-
+    // arr는 
     
     return(
         <>  <form name="formTag" onSubmit={(e)=>{
             e.preventDefault();
             dispatch(onSave({title:e.target.body.value, id: Date.now()}));
-            // 왜 타이틀인가.
+            // 내가 보내주는 형식으로 받는 쪽에서도 받는다. 둘 이상이므로 객체로 보내는 것이 좋다.
+            // 아이디는 필요한 양식이다.
         }}>
-            {/* 타이틀: e.target.으로 해야 함.그래야 payload형식을 맞추게 됨. */}
                 <input type="text" name="body"/>
                 <input type="submit" value="누르기"/>
-                {/* submit 주의 */}
             </form>
 
             <ul>
                 {arr.map((item, index) => (
                     <li key={index}>{item.title}<button id={index} onClick={(e)=>{
                         dispatch(onDelete({id: item.id}));
-                        //객체 형태로 넘겨주자. 
-                }
-                }>삭제</button></li>))}
+                        // 여기서 id는 각 arr배열의 id를 뜻하게 된다.
+                        // 배열로 생성은 끝났지만, 클로저라는 기능으로 온클릭 이벤트 핸들러가 그때의 아이디를 기억하고 있다고 한다. 
+                }}>삭제</button></li>))}
             </ul>
-            {/* 맵을 이용하여 작성.  */}
-
         </>
     );
 }
--- ex-app/src/couterSlice.js
+++ ex-app/src/couterSlice.js
@@ -1,47 +1,28 @@
 import {createSlice} from "@reduxjs/toolkit";
-const initState=[
-        // {title:전국일주},
-        // {title:해외일주},
-        // {title:기타내용}
-];
+const initState=[];
 const counterSlice=createSlice({
     name: 'counter',
     //네임
     initialState:{
         wishList:initState,
-        // 이렇게 땡땡으로 처리해도 된다고 한다.
+        // 새로운 배열명: 기존 배열명 하면 대치가 된다고 한다. 
     },
     // 초기 상태
     reducers:{
         onSave: (state,action)=>{
             state.wishList.push({...action.payload});
-            console.log("action");
-            console.log(action);
-            console.log("payload");
-            console.log(action.payload);
-            console.log("{...action.payload}");
-            console.log({...action.payload});
             //{title:action.payload.title , id:action.payload.id}
-            // 기본으로 state는 보내고, action은 뭔가 받을 목적으로
-            // 부가정보는.payload라고 쓴다.
-            // 보내는 듯 하다.
-            // state 다음은 초기변수의 이름 명.
-            // state 안에 있는 것은 이니셜 스테이트 안에 있는 것이여야 함.
+            // 본래라면 push만 하면 렌더링이 일어나지 않는데, redux의 툴킷 작용으로 새로운 객체 생성의 영향을 미쳐 이루어진다고 함.
         },    
         onDelete:(state,action)=>{
             state.wishList=state.wishList.filter((e)=>( e.id!==action.payload.id));
+            
         }
-        // (액션생성자이름,콜론,띄어쓰기)하고 시작.
-        // 스테이트는 내가 가진 초기 변수이고, 액션은 들어갈 타입이다.
-        // 원래 상태 값은 막 못바꾸는데 리듀서에서는 바꿀 수 있는 모양이다.
     }
 });
-// 이 안에 3가지 요소를 넣는다.
 
 export const {onSave,onDelete}=counterSlice.actions;
-// 오른쪽에는 객체들, 왼쪽에는 개별 속성명, 슬라이스.액션스가 각 리듀서 함수이다.
-// 객체분해로 변수할당, 액션생성자를 보내는 것이고, 이게 있어야 디스패치로 액션 타입을 객체를 받아서
-// 해당 컴퍼넌트가 영향을 받을 수 있다.
+// 객체분해 할당으로 액션 생성자를  보내는 것이고, 이를 통해 디스패치로 액션의 타입을 객체로 받아서
+// 해당 컴퍼넌트가 영향을 받게 된다.
 export default counterSlice;
-
-//.actions가 붙은 액션 생성자를 보낸후, .reducer 가 붙은 리듀서를 보내 리듀서 함수를 받게 한다.
+// 카운터 슬라이스는 안에 리듀서랑 액션생성자를 속성이름으로 가지고 있다. 리듀서를 위해서 카운터 슬라이스를 보낸다.
